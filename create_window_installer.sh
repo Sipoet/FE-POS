343948722a02048055c1f3ce84a8049d58bf008b
reidy <slayoslay@gmail.com>
2023-12-15 12:34:52 +0800

    use signtools for add certificate instead

--- create_window_installer.sh
+++ create_window_installer.sh
@@ -1,2 +1,3 @@
-dart run msix:create
+dart run msix:create -y --certificate-path .\assets\certs\CERTIFICATE.pfx --certificate-password "your_cert_password"
+# flutter pub run msix:create
 dart run msix:publish
