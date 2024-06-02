10830ef9d66124a433473a4fe22cbb8fcdb0d8c5
reidy <slayoslay@gmail.com>
2024-06-02 19:36:29 +0800

    add gross profit on sales percentage report

--- lib/model/item_sales_percentage_report.dart
+++ lib/model/item_sales_percentage_report.dart
@@ -17,6 +17,7 @@ class ItemSalesPercentageReport extends Model {
   Money salesTotal;
   int numberOfPurchase;
   Money purchaseTotal;
+  Money grossProfit;
   int itemOut;
   DateTime? recentPurchaseDate;
 
@@ -39,6 +40,7 @@ class ItemSalesPercentageReport extends Model {
       required this.numberOfPurchase,
       required this.purchaseTotal,
       required this.itemOut,
+      required this.grossProfit,
       this.recentPurchaseDate});
   @override
   factory ItemSalesPercentageReport.fromJson(Map<String, dynamic> json) {
@@ -55,13 +57,17 @@ class ItemSalesPercentageReport extends Model {
         warehouseStock: attributes['warehouse_stock'],
         brand: attributes['brand_name'],
         percentageSales: Percentage(attributes['percentage_sales']),
-        sellPrice: Money(attributes['sell_price']),
-        avgBuyPrice: Money(attributes['avg_buy_price']),
+        sellPrice: Money.tryParse(attributes['sell_price']) ?? const Money(0),
+        avgBuyPrice:
+            Money.tryParse(attributes['avg_buy_price']) ?? const Money(0),
         numberOfSales: attributes['number_of_sales'],
-        salesTotal: Money(attributes['sales_total']),
+        salesTotal: Money.tryParse(attributes['sales_total']) ?? const Money(0),
         numberOfPurchase: attributes['number_of_purchase'],
         itemOut: attributes['item_out'],
-        purchaseTotal: Money(attributes['purchase_total']),
+        purchaseTotal:
+            Money.tryParse(attributes['purchase_total']) ?? const Money(0),
+        grossProfit:
+            Money.tryParse(attributes['gross_profit']) ?? const Money(0),
         recentPurchaseDate:
             DateTime.tryParse(attributes['recent_purchase_date'] ?? ''));
   }
@@ -86,5 +92,6 @@ class ItemSalesPercentageReport extends Model {
         'purchase_total': purchaseTotal,
         'recent_purchase_date': recentPurchaseDate,
         'item_out': itemOut,
+        'gross_profit': grossProfit,
       };
 }
