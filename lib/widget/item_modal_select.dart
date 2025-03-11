fa05a0b02734aff437f03ecbd6a69e481f22f9b4
reidy <slayoslay@gmail.com>
2025-03-11 21:27:11 +0800

    test

--- lib/widget/item_modal_select.dart
+++ lib/widget/item_modal_select.dart
@@ -24,7 +24,7 @@ class _ItemModalSelectState extends State<ItemModalSelect> {
   }
 
   void fetchItem() {
-    _server.get('item_reports/')
+    _server.get('item_reports/', queryParam: {});
   }
 
   @override
