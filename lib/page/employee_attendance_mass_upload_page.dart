03db3c2ed329b31420f278ce87504accc78556f1
reidy <slayoslay@gmail.com>
2025-10-23 14:49:58 +0800

    change table result that support filter

--- lib/page/employee_attendance_mass_upload_page.dart
+++ lib/page/employee_attendance_mass_upload_page.dart
@@ -1,13 +1,14 @@
+import 'package:fe_pos/model/item_report.dart';
 import 'package:fe_pos/tool/default_response.dart';
 import 'package:fe_pos/tool/flash.dart';
 import 'package:fe_pos/tool/loading_popup.dart';
 import 'package:fe_pos/tool/platform_checker.dart';
 import 'package:fe_pos/tool/text_formatter.dart';
+import 'package:fe_pos/widget/sync_data_table.dart';
 import 'package:flutter/material.dart';
 import 'package:fe_pos/model/employee_attendance.dart';
 import 'package:fe_pos/model/session_state.dart';
 
-import 'package:fe_pos/tool/setting.dart';
 import 'package:file_picker/file_picker.dart';
 
 import 'package:provider/provider.dart';
@@ -29,15 +30,49 @@ class _EmployeeAttendanceMassUploadPageState
         TextFormatter,
         DefaultResponse {
   late Server _server;
-  late Setting _setting;
-  late final EmployeeAttendanceMassUploadDatatableSource _source;
+  PlutoGridStateManager? _source;
+  late final List<TableColumn> _columns;
   final _focusNode = FocusNode();
-  List<bool> selected = [];
+
   @override
   void initState() {
     _server = context.read<Server>();
-    _setting = context.read<Setting>();
-    _source = EmployeeAttendanceMassUploadDatatableSource(setting: _setting);
+    _columns = [
+      TableColumn(
+          clientWidth: 200,
+          name: 'employee.name',
+          humanizeName: 'Nama Karyawan'),
+      TableColumn(
+        clientWidth: 150,
+        name: 'date',
+        humanizeName: 'Tanggal',
+        type: TableColumnType.date,
+      ),
+      TableColumn(
+        clientWidth: 120,
+        name: 'start_work',
+        humanizeName: 'Jam Masuk',
+        type: TableColumnType.timeOnly,
+      ),
+      TableColumn(
+        clientWidth: 120,
+        name: 'end_work',
+        humanizeName: 'Jam Keluar',
+        type: TableColumnType.timeOnly,
+      ),
+      TableColumn(
+        clientWidth: 100,
+        name: 'shift',
+        humanizeName: 'Shift',
+        type: TableColumnType.number,
+      ),
+      TableColumn(
+        clientWidth: 120,
+        name: 'is_late',
+        humanizeName: 'Terlambat?',
+        type: TableColumnType.boolean,
+      ),
+    ];
     super.initState();
     Future.delayed(Duration.zero, () => _focusNode.requestFocus());
   }
@@ -67,120 +102,29 @@ class _EmployeeAttendanceMassUploadPageState
                 focusNode: _focusNode,
                 child: const Text('Pilih file')),
           ),
-          Visibility(
-            visible: _source.rows.isNotEmpty,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 10,
+          Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Hasil :',
+                style: headerStyle,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: bodyScreenHeight,
+                child: SyncDataTable<ItemReport>(
+                  columns: _columns,
+                  onLoaded: (stateManager) => _source = stateManager,
+                  showFilter: true,
+                  showSummary: false,
+                  isPaginated: true,
                 ),
-                const Text(
-                  'Hasil :',
-                  style: headerStyle,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: SizedBox(
-                    width: 1100,
-                    child: PaginatedDataTable(
-                      showFirstLastButtons: true,
-                      rowsPerPage: 30,
-                      showCheckboxColumn: false,
-                      sortAscending: _source.isAscending,
-                      sortColumnIndex: _source.sortColumn,
-                      columns: [
-                        DataColumn(
-                          label:
-                              const Text('Nama Karyawan', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows.sort((a, b) =>
-                                a.employee.name.compareTo(b.employee.name) *
-                                num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                        DataColumn(
-                          label: const Text('Tanggal', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows
-                                .sort((a, b) => a.date.compareTo(b.date) * num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                        DataColumn(
-                          label: const Text('Shift', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows.sort(
-                                (a, b) => a.shift.compareTo(b.shift) * num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                        DataColumn(
-                          label: const Text('Jam Masuk', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows.sort((a, b) =>
-                                a.startTime.compareTo(b.startTime) * num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                        DataColumn(
-                          label: const Text('Jam Keluar', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows.sort(
-                                (a, b) => a.endTime.compareTo(b.endTime) * num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                        DataColumn(
-                          label: const Text('Terlambat?', style: headerStyle),
-                          onSort: (columnIndex, isAscending) {
-                            final num = isAscending ? 1 : -1;
-                            _source.rows.sort((a, b) =>
-                                a.isLate
-                                    .toString()
-                                    .compareTo(b.isLate.toString()) *
-                                num);
-                            setState(() {
-                              _source.sortColumn = columnIndex;
-                              _source.isAscending = isAscending;
-                              _source.setData(_source.rows);
-                            });
-                          },
-                        ),
-                      ],
-                      source: _source,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
           const SizedBox(
             height: 10,
@@ -188,40 +132,6 @@ class _EmployeeAttendanceMassUploadPageState
         ],
       ),
     ));
-  }
-
-  Future createOrUpdateEmployeeAttendance(
-      EmployeeAttendance employeeAttendance, int index) async {
-    Map body = {'employee_attendance': employeeAttendance};
-    dynamic request;
-    if (employeeAttendance.id == null) {
-      request = await _server.post('employee_attendances', body: body);
-    } else {
-      request = await _server
-          .put('employee_attendances/${employeeAttendance.id}', body: body);
-    }
-    if ([200, 201].contains(request.statusCode)) {
-      var data = request.data['data'];
-      setState(() {
-        if (employeeAttendance.id == null) {
-          employeeAttendance.id = int.tryParse(data['id']);
-          employeeAttendance.employee = Employee(
-              id: data['attributes']['id'],
-              code: 'code',
-              name: data['attributes']['employee_name'],
-              role: Role(name: ''),
-              startWorkingDate: Date.today());
-        }
-        _source.selected[index] = false;
-        _source.setStatus(index, 'saved');
-      });
-    } else {
-      setState(() {
-        _source.setStatus(index, 'failed');
-      });
-    }
-
-    return request;
   }
 
   void pickFile() async {
@@ -251,7 +161,7 @@ class _EmployeeAttendanceMassUploadPageState
               .map<EmployeeAttendance>((json) => EmployeeAttendanceClass()
                   .fromJson(json, included: response.data['included']))
               .toList();
-          _source.setData(employeeAttendances);
+          _source?.setModels(employeeAttendances, _columns);
         });
       } else {
         final flash = Flash();
@@ -264,63 +174,4 @@ class _EmployeeAttendanceMassUploadPageState
       defaultErrorResponse(error: error);
     }).whenComplete(() => hideLoadingPopup());
   }
-}
-
-class EmployeeAttendanceMassUploadDatatableSource extends DataTableSource
-    with TextFormatter {
-  List<EmployeeAttendance> rows = [];
-  List selected = [];
-  List status = [];
-  final Setting setting;
-  bool isAscending = true;
-  int sortColumn = 0;
-
-  EmployeeAttendanceMassUploadDatatableSource({required this.setting});
-
-  void setData(data) {
-    rows = data;
-    selected = List.generate(rows.length, (index) => true);
-    status = List.generate(rows.length, (index) => 'Draft');
-    notifyListeners();
-  }
-
-  void setStatus(index, newStatus) {
-    status[index] = newStatus;
-    notifyListeners();
-  }
-
-  @override
-  int get rowCount => rows.length;
-
-  @override
-  DataRow? getRow(int index) {
-    return DataRow(
-      key: ObjectKey(rows[index]),
-      cells: decorateEmployeeAttendance(index),
-      selected: selected[index],
-      onSelectChanged: (bool? value) {
-        selected[index] = value!;
-        notifyListeners();
-      },
-    );
-  }
-
-  List<DataCell> decorateEmployeeAttendance(int index) {
-    final employeeAttendance = rows[index];
-    return <DataCell>[
-      DataCell(SelectableText(employeeAttendance.employee.name)),
-      DataCell(SelectableText(dateFormat(employeeAttendance.date))),
-      DataCell(SelectableText(employeeAttendance.shift.toString())),
-      DataCell(
-          SelectableText(dateTimeLocalFormat(employeeAttendance.startTime))),
-      DataCell(SelectableText(dateTimeLocalFormat(employeeAttendance.endTime))),
-      DataCell(SelectableText(employeeAttendance.isLate.toString())),
-    ];
-  }
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => 0;
 }
